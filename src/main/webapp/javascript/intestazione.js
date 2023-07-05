d68e480c7982db64dbc67f3584c9ec599825a939
barbomattia <mattiabarbo2002@gmail.com>
2023-07-05 20:29:40 +0200

    pagina aderente iscrizioni e menu profilo

--- src/main/webapp/javascript/intestazione.js
+++ src/main/webapp/javascript/intestazione.js
@@ -2,6 +2,7 @@ hamburger = document.querySelector('.icon_hamburger');
 menu = document.getElementById("menu")
 el1 = document.querySelector('.el1')
 el2 = document.querySelector('.el2')
+subMenu = document.getElementById("subMenu");
 
 hamburger.addEventListener("click", function (){
     let open = menu.getAttribute("menu_open")
@@ -11,3 +12,7 @@ hamburger.addEventListener("click", function (){
         menu.setAttribute("menu_open","true")
     }
 } )
+
+function toggleMenu(){
+    subMenu.classList.toggle("open-menu");
+}
---
+++ src/main/webapp/profilo.jsp
@@ -0,0 +1,17 @@
+<jsp:include page="intestazione.jsp" />
+
+<link rel="stylesheet" type="text/css" href="css/struttura.css">
+<link rel="stylesheet" type="text/css" href="css/profilo.css">
+
+<main>
+
+  <section>
+    <div>
+
+    </div>
+  </section>
+
+</main>
+
+<jsp:include page="footer.jsp"/>
+
