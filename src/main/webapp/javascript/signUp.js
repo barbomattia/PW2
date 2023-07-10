bcd8e9129ad876150d9a43fa956c0978cf670863
matteogrisenti <matteo.grisenti@studenti.unitn.it>
2023-07-10 12:18:36 +0200

    Sistemanzione Pagina Registrazione

--- src/main/webapp/javascript/signUp.js
+++ src/main/webapp/javascript/signUp.js
@@ -1,3 +1,16 @@
+form = document.getElementById("formSignUp");
+
+titolo_errore = document.createElement("h3")
+titolo_errore.style.color = "red"
+titolo_errore.style.textAlign ="center"
+titolo_errore.style.marginTop = "10px"
+titolo_errore.style.marginBottom = "0"
+
+testo_errore = document.createElement("p")
+testo_errore.style.color = "red"
+testo_errore.style.textAlign ="center"
+testo_errore.style.margin ="0"
+
 function mostraOrNascondiPassword(id){
     let password = document.getElementById(id);
     if(password.type === "password"){
@@ -10,31 +23,51 @@ function mostraOrNascondiPassword(id){
 function controllaCampi(){
     let check = true;
     if(!isMaggiorenne()){
-        console.log("L'utente non è maggiorenne");
-        window.alert("Errore: devi essere maggiorenne per iscriverti");
+        //console.log("L'utente non è maggiorenne");
+        titolo_errore.innerText="21:Non Maggiorenne"
+        testo_errore.innerText="devi essere maggiorenne per iscriverti"
+        evidenziaErrore("data");
+        //window.alert("Errore: devi essere maggiorenne per iscriverti");
         check = false;
     }
     if(!isNumeroDiTelefono()){
-        console.log("Il numero di telefono inserito ha un formato non valido");
-        window.alert("Errore: numero di telefono inserito con un formato invalido");
+        //console.log("Il numero di telefono inserito ha un formato non valido");
+        titolo_errore.innerText="21: Numero di Telefono Invalido"
+        testo_errore.innerText="numero di telefono inserito con un formato invalido"
+        evidenziaErrore("telefono");
+        //window.alert("Errore: numero di telefono inserito con un formato invalido");
         check = false;
     }
     if(!isMail()){
-        console.log("La mail inserita ha un formato non valido");
-        window.alert("Errore: mail inserita con un formato invalido");
+        //console.log("La mail inserita ha un formato non valido");
+        titolo_errore.innerText="21: Mail Invalida"
+        testo_errore.innerText="mail inserita con un formato invalido"
+        evidenziaErrore("mail");
+        //window.alert("Errore: mail inserita con un formato invalido");
         check = false;
     }
     if(!passwordIdentiche()){
-        console.log("Le due password inserite non corrispondono");
-        window.alert("Errore: le password inserite non corrispondono");
+        //console.log("Le due password inserite non corrispondono");
+        titolo_errore.innerText="21: Password Non Corrispondenti"
+        testo_errore.innerText="le password inserite non corrispondono"
+        evidenziaErrore("password");
+        //window.alert("Errore: le password inserite non corrispondono");
         check = false;
     }
     if(!passwordCorretta()){
-        console.log("La password non rispetta le caratteristiche richieste");
-        window.alert("Errore: la password non rispetta le caratteristiche richieste");
+        //console.log("La password non rispetta le caratteristiche richieste");
+        titolo_errore.innerText="21: Password Invalida"
+        testo_errore.innerText="la password non rispetta le caratteristiche richieste"
+        evidenziaErrore("password");
+        //window.alert("Errore: la password non rispetta le caratteristiche richieste");
         check = false;
     }
+
     if(!check){
+
+        form.appendChild(titolo_errore)
+        form.appendChild(testo_errore)
+
         //Se almeno una delle condizioni non è stata rispettata (mi dice che event è deprecato ma non ho capito il perché)
         event.preventDefault();
 
@@ -43,6 +76,7 @@ function controllaCampi(){
         parole, viene utilizzata per impedire l'azione predefinita associata a un
         determinato evento.
          */
+
     }
     return check;
 }
@@ -128,4 +162,71 @@ function passwordCorretta(){
 
     //Non ho riscontrato errori nella formattazione della password
     return true;
+}
+
+function evidenziaErrore(input){
+    if(input=="data"){
+        let input = document.getElementById("idDataDiNascita");
+        input.setAttribute("error","true")
+
+        let input_label = document.getElementById("idDataDiNascita_label")
+        input_label.setAttribute("error_label","true")
+    }
+
+    if(input=="telefono"){
+        let input = document.getElementById("idNumeroDiTelefono");
+        input.setAttribute("error","true")
+
+        let input_label = document.getElementById("idNumeroDiTelefono_label")
+        input_label.setAttribute("error_label","true")
+    }
+
+    if(input=="mail"){
+        let input = document.getElementById("idMail");
+        input.setAttribute("error","true")
+
+        let input_label = document.getElementById("idMail_label")
+        input_label.setAttribute("error_label","true")
+    }
+
+    if(input=="password"){
+        let input1 = document.getElementById("idPassword1");
+        input1.setAttribute("error","true")
+
+        let input2 = document.getElementById("idPassword2");
+        input2.setAttribute("error","true")
+
+        let input_label1 = document.getElementById("idPassword1_label")
+        input_label1.setAttribute("error_label","true")
+
+        let input_label2 = document.getElementById("idPassword2_label")
+        input_label2.setAttribute("error_label","true")
+    }
+}
+
+function over_reset(){
+    reset_style()
+    form.reset();
+}
+
+function reset_style(){
+
+    let inputs_elements = form.getElementsByTagName("input");
+    for (let input_element of inputs_elements) {
+        input_element.setAttribute("error","false")
+    }
+
+    let label_elements = form.getElementsByTagName("label");
+    for (let label_element of label_elements) {
+        label_element.setAttribute("error_label","false")
+    }
+
+    if(form.contains(testo_errore)){
+        //console.log(form.children)
+        form.removeChild(testo_errore)
+    }
+
+    if(form.contains(titolo_errore)){
+        form.removeChild(titolo_errore)
+    }
 }
--- src/main/webapp/signUp.jsp
+++ src/main/webapp/signUp.jsp
@@ -9,55 +9,49 @@
     <section class="signUp">
         <div class="riquadro">
             <h2>Registrazione</h2>
-            <form class="form" onsubmit="return controllaCampi()" action="${pageContext.request.contextPath}/signUp" method="POST">
+            <form id="formSignUp" class="form" onsubmit="return controllaCampi()" onreset="over_reset()" action="${pageContext.request.contextPath}/signUp" method="POST">
                 <div class="column">
                     <div class="inputRegistrazione">
-                        <label>Nome</label>
-                        <label>
-                            <input type="text" placeholder="Nome" required name="name">
-                        </label>
+                        <label error_label="false">Nome</label>
+                        <input type="text" placeholder="Nome" required name="name" error="false">
                     </div>
                     <div class="inputRegistrazione">
-                        <label>Cognome</label>
-                        <label>
-                            <input type="text" placeholder="Cognome" required name="surname">
-                        </label>
+                        <label error_label="false">Cognome</label>
+                        <input type="text" placeholder="Cognome" required name="surname" error="false">
                     </div>
                 </div>
 
                 <div class="column">
                     <div class="inputRegistrazione">
-                        <label>Email</label>
-                        <label for="idMail"></label><input type="email" placeholder="Email" required name="mail" id="idMail">
+                        <label for="idMail" id="idMail_label" error_label="false">Email</label>
+                        <input type="email" placeholder="Email" required name="mail" id="idMail" error="false">
                     </div>
                     <div class="inputRegistrazione">
-                        <label>Username</label>
-                        <label>
-                            <input type="text" placeholder="Username" required name="username">
-                        </label>
+                        <label id="idUsername_label" error_label="false">Username</label>
+                        <input type="text" placeholder="Username" required name="username" id="idUsername" error="false">
                     </div>
                 </div>
 
                 <div class="column">
                     <div class="inputRegistrazione">
-                        <label>Telefono</label>
-                        <label for="idNumeroDiTelefono"></label><input type="Number" name="phone_number" placeholder="Numero di telefono" required id="idNumeroDiTelefono">
+                        <label for="idNumeroDiTelefono" id="idNumeroDiTelefono_label" error_label="false">Telefono</label>
+                        <input type="Number" name="phone_number" placeholder="Numero di telefono" required id="idNumeroDiTelefono" error="false">
                     </div>
                     <div class="inputRegistrazione">
-                        <label>Data di nascita</label>
-                        <label for="idDataDiNascita"></label><input type="date" name="birth" placeholder="Data di nascita" required id="idDataDiNascita">
+                        <label for="idDataDiNascita" id="idDataDiNascita_label" error_label="false">Data di nascita</label>
+                        <input type="date" name="birth" placeholder="Data di nascita" required id="idDataDiNascita" error="false">
                     </div>
                 </div>
 
                 <div class="column">
                     <div class="inputRegistrazione">
-                        <label>Password</label>
-                        <label for="idPassword1"></label><input type="password" placeholder="Password" required name="password" id="idPassword1">
+                        <label for="idPassword1" id="idPassword1_label" error_label="false">Password</label>
+                        <input type="password" placeholder="Password" required name="password" id="idPassword1" error="false">
                         <button type="button" class="button-standard" onclick="mostraOrNascondiPassword('idPassword1')">Mostra/Nascondi</button>
                     </div>
                     <div class="inputRegistrazione">
-                        <label>Conferma Password</label>
-                        <label for="idPassword2"></label><input type="password" placeholder="Conferma password" required id="idPassword2">
+                        <label for="idPassword2" id="idPassword2_label" error_label="false">Conferma Password</label>
+                        <input type="password" placeholder="Conferma password" required id="idPassword2" error="false">
                         <button type="button" class="button-standard" onclick="mostraOrNascondiPassword('idPassword2')">Mostra/Nascondi</button>
                     </div>
                 </div>
